b42dba400c43a11724c8ecf5897e4732d4e99adf
tengu <karasuyamatengu@gmail.com>
2014-02-07 21:49:07 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version = "0.1",
     url = "https://github.com/tengu/py-mail2json",
     description = "convert email to json",
-    packages = ['mail2json'],
+    py_modules = ['mail2json'],
     entry_points={"console_scripts": [ 'mail2json=mail2json:main' ]},
     install_requires=['baker'],
     zip_safe=False,
